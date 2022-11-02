267267512b3926213e4905ad1216a8807d511b4c
Ane <anesoejoe@gmail.com>
2022-11-02 09:04:23 +0100

    Refactor

--- GraphManipulation/Program.cs
+++ GraphManipulation/Program.cs
@@ -4,51 +4,60 @@ using System.Data.SQLite;
 using GraphManipulation.Models.Stores;
 using VDS.RDF;
 using VDS.RDF.Parsing;
-using VDS.RDF.Writing;
 using VDS.RDF.Shacl;
 using VDS.RDF.Shacl.Validation;
+using VDS.RDF.Writing;
 
+namespace GraphManipulation;
 
-string baseUri = "http://www.test.com/";
-
-using var conn = new SQLiteConnection("Data Source=/home/ane/Documents/GitHub/Legeplads/Databases/OptimizedAdvancedDatabase.sqlite");
-
-var sqlite = new Sqlite("", baseUri, conn);
-
-sqlite.Build();
-
-IGraph graph = sqlite.ToGraph();
-
-var writer = new CompressingTurtleWriter();
-const string path = "/home/ane/Documents/GitHub/GraphManipulation/GraphManipulation/test.ttl";
-writer.Save(graph, path);
-
-IGraph dataGraph = new Graph();
-dataGraph.LoadFromFile(path);
-
-IGraph ontology = new Graph();
-const string ontologyPath = "/home/ane/Documents/GitHub/GraphManipulation/GraphManipulation/Ontologies/datastore-description-language.ttl";
-ontology.LoadFromFile(ontologyPath, new TurtleParser());
-var shapesGraph = new ShapesGraph(ontology);
-
-PrintReport(shapesGraph.Validate(dataGraph));
-
-
-void PrintReport(Report report)
+public static class Program
 {
-    var message = "";
-
-    message += "\nConforms: " + report.Conforms + " (" + report.Results.Count + ")";
-
-    foreach (var result in report.Results)
+    public static void Main()
     {
-        message += result.FocusNode is not null ? "\nFocus node: " + result.FocusNode.ToString() : "";
-        message += result.ResultPath is not null ? "\nResult path: " + result.ResultPath : "";
-        message += result.ResultValue is not null ? "\nResult value: " + result.ResultValue.ToString() : "";
-        message += "\nMessage: " + result.Message.Value + "\n";
+        CreateAndValidateGraph();
     }
 
-    Console.WriteLine(message);
+    private static void CreateAndValidateGraph()
+    {
+        string baseUri = "http://www.test.com/";
+
+        using var conn = new SQLiteConnection("Data Source=/home/ane/Documents/GitHub/Legeplads/Databases/OptimizedAdvancedDatabase.sqlite");
+
+        var sqlite = new Sqlite("", baseUri, conn);
+
+        sqlite.Build();
+
+        IGraph graph = sqlite.ToGraph();
+
+        var writer = new CompressingTurtleWriter();
+        const string path = "/home/ane/Documents/GitHub/GraphManipulation/GraphManipulation/test.ttl";
+        writer.Save(graph, path);
+
+        IGraph dataGraph = new Graph();
+        dataGraph.LoadFromFile(path);
+
+        IGraph ontology = new Graph();
+        const string ontologyPath = "/home/ane/Documents/GitHub/GraphManipulation/GraphManipulation/Ontologies/datastore-description-language.ttl";
+        ontology.LoadFromFile(ontologyPath, new TurtleParser());
+        var shapesGraph = new ShapesGraph(ontology);
+
+        PrintReport(shapesGraph.Validate(dataGraph));
+    }
+
+    private static void PrintReport(Report report)
+    {
+        var message = "";
+
+        message += "\nConforms: " + report.Conforms + (report.Results.Count == 0 ? "" : " (" + report.Results.Count + ")");
+
+        foreach (var result in report.Results)
+        {
+            message += result.FocusNode is not null ? "\nFocus node: " + result.FocusNode.ToString() : "";
+            message += result.ResultPath is not null ? "\nResult path: " + result.ResultPath : "";
+            message += result.ResultValue is not null ? "\nResult value: " + result.ResultValue.ToString() : "";
+            message += "\nMessage: " + result.Message.Value + "\n";
+        }
+
+        Console.WriteLine(message);
+    }
 }
-
-
