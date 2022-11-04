b6e6137b561895f19b03f8da4a099b794aabc45b
Ane <anesoejoe@gmail.com>
2022-11-04 15:18:05 +0100

    Removed redundant code

--- GraphManipulation/Extensions/GraphAsserts.cs
+++ GraphManipulation/Extensions/GraphAsserts.cs
@@ -16,35 +16,6 @@ public static class GraphAsserts
         graph.Assert(subj, pred, obj);
     }
 
-    // public static void AssertSqliteTriple(this IGraph graph, Entity entity)
-    // {
-    //     graph.AssertTypeTriple(entity, typeof(Sqlite));
-    // }
-    //
-    // public static void AssertSchemaTriple(this IGraph graph, Entity entity)
-    // {
-    //     graph.AssertTypeTriple(entity, typeof(Schema));
-    // }
-    //
-    // public static void AssertTableTriple(this IGraph graph, Entity entity)
-    // {
-    //     graph.AssertTypeTriple(entity, typeof(Table));
-    // }
-    //
-    // public static void AssertColumnTriple(this IGraph graph, Entity entity)
-    // {
-    //     graph.AssertTypeTriple(entity, typeof(Column));
-    // }
-
-    // public static void AssertTypeTriple(this IGraph graph, Entity entity, Type type)
-    // {
-    //     var subj = graph.CreateUriNode(entity.Uri);
-    //     var pred = graph.CreateUriNode("rdf:type");
-    //     var obj = graph.CreateUriNode(GraphDataType.GetGraphTypeString(type));
-    //
-    //     graph.Assert(subj, pred, obj);
-    // }
-
     public static void AssertTypeTriple(this IGraph graph, Entity entity)
     {
         var subj = graph.CreateUriNode(entity.Uri);
