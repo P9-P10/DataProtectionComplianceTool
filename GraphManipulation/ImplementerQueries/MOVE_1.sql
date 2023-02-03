543c93d7961fef9adc36fb6a97f0fc149ce82a83
Ane <anesoejoe@gmail.com>
2023-02-03 14:11:56 +0100

    Some Implementer stuff

--- GraphManipulation/ImplementerQueries/MOVE_1.sql
+++ GraphManipulation/ImplementerQueries/MOVE_1.sql
@@ -1,10 +1,19 @@
 --- Ikke udfyld rækker, ikke indsæt default værdier
 
+DROP VIEW IF EXISTS MOVE_1_temp;
 DROP VIEW IF EXISTS MOVE_1;
 
-CREATE VIEW MOVE_1 AS
-SELECT U.id, U.name, U.email, subscribed
+CREATE VIEW MOVE_1_temp AS
+SELECT U.*, subscribed
 FROM Users as U
          LEFT JOIN MarketingInformation as M on U.id = M.id;
+
+SELECT * FROM MOVE_1_temp;
+
+CREATE VIEW MOVE_1 AS
+SELECT M.*,
+       CASE WHEN t.id is null THEN FALSE ELSE TRUE END AS originally_in_MarketingInformation
+FROM MOVE_1_temp AS M LEFT JOIN (SELECT id from MarketingInformation) as t
+ON M.id = t.id;
 
 SELECT * FROM MOVE_1;
--- GraphManipulation/ImplementerQueries/MOVE_2.sql
+++ GraphManipulation/ImplementerQueries/MOVE_2.sql
@@ -1,13 +1,27 @@
 --- Ikke udfyld rækker, indsæt default værdier
 
 DROP VIEW IF EXISTS MOVE_1;
+DROP VIEW IF EXISTS MOVE_1_temp;
 DROP VIEW IF EXISTS MOVE_2;
 
 
-CREATE VIEW MOVE_1 AS
-SELECT U.id, name, U.email, subscribed
+CREATE VIEW MOVE_1_temp AS
+SELECT U.id, U.name, U.email, subscribed
 FROM Users as U
          LEFT JOIN MarketingInformation as M on U.id = M.id;
+
+SELECT *
+FROM MOVE_1_temp;
+
+CREATE VIEW MOVE_1 AS
+SELECT M.id,
+       name,
+       email,
+       subscribed,
+       CASE WHEN t.id is null THEN FALSE ELSE TRUE END AS originally_in_MarketingInformation
+FROM MOVE_1_temp AS M
+         LEFT JOIN (SELECT id from MarketingInformation) as t
+                   ON M.id = t.id;
 
 CREATE VIEW MOVE_2 AS
 SELECT id,
@@ -16,7 +30,9 @@ SELECT id,
        CASE
            WHEN subscribed THEN TRUE
            ELSE FALSE
-           END AS subscribed
+           END AS subscribed,
+       originally_in_MarketingInformation
 FROM MOVE_1;
 
-SELECT * FROM MOVE_2;
+SELECT *
+FROM MOVE_2;
