b3ce336b338421ae3291a947e6f1dd9493e6a361
Jakob Sønderby Kristensen <33911621+soenderby@users.noreply.github.com>
2023-03-21 14:13:39 +0100

    Moved all metadata related classes into a single directory

--- GraphManipulation/MetadataManagement/InsertStatementBuilder.cs
+++ GraphManipulation/MetadataManagement/InsertStatementBuilder.cs
@@ -30,30 +30,11 @@ public class InsertStatementBuilder
         }
 
         string valuesString =  string.Join(", ", valuesAsStrings);
-        
-        
+
         // Build string of the form (<column1>, <column2> ..., <column3>) 
         // The column names are defined by the property names of InsertValues
         string columnsString = $"({string.Join(", ", columns)})";
-
         
         return $"INSERT INTO {Table} {columnsString} VALUES({valuesString});";
-    }
-
-    private string GetValuesString(PropertyInfo[] props)
-    {
-        //props.Select(prop => prop.GetValue(InsertValues, null).ToString());
-        List<string> valuesAsStrings = new List<string>();
-        foreach (PropertyInfo prop in props)
-        {
-            object value = prop.GetValue(InsertValues, null);
-            if (value == null) continue;
-            valuesAsStrings.Add(
-                value is string ? 
-                    $"'{value}'" // If the value is a string it must be surrounded by singlequotes
-                    : value.ToString().ToLower()); // Otherwise it must be converted to a lowercase string. 
-        }
-
-        return string.Join(", ", valuesAsStrings);
     }
 }
