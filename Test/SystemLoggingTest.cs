7b04ac3772994263ff81da87015f7fbfcb7826f4
Ane <anesoejoe@gmail.com>
2023-03-31 13:28:57 +0200

    Started devising system logging tests

---
+++ Test/SystemLoggingTest.cs
@@ -0,0 +1,21 @@
+using Xunit;
+
+namespace Test;
+
+public class SystemLoggingTest
+{
+    public class MetadataManagement
+    {
+        
+    }
+
+    public class Vacuuming
+    {
+        
+    }
+    
+    public class SchemaEvolution
+    {
+        
+    }
+}
