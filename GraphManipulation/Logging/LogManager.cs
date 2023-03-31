f5ab1bea32ab328fe7c3cf92fa2bdfd30062b68c
Ane <anesoejoe@gmail.com>
2023-03-31 13:22:57 +0200

    Proposal

---
+++ GraphManipulation/Logging/LogManager.cs
@@ -0,0 +1,7 @@
+namespace GraphManipulation.Logging;
+
+public class LogManager
+{
+    // Måske vi skal have en log manager, som er den der holder styr på hvilke filer der logges i?
+    // Den kunne så bruge Loggeren til at udføre sine opgaver.
+}
