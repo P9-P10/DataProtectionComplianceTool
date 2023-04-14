35ca4afe9d83cd36dd912b49bbe2e10783ca642a
Ane <anesoejoe@gmail.com>
2023-04-14 16:12:16 +0200

    Renamed again

--- GraphManipulation/Managers/Interfaces/ILinkManager.cs
+++ GraphManipulation/Managers/Interfaces/ILinkManager.cs
@@ -2,8 +2,8 @@ namespace GraphManipulation.Managers.Interfaces;
 
 public interface ILinkManager<T1, T2>
 {
-    public void AddLink(T1 key1, T2 key2);
-    public void DeleteLink(T1 key1, T2 key2);
-    public IEnumerable<T1> GetLinkedEntities(T2 key);
-    public IEnumerable<T2> GetLinkedEntities(T1 key);
+    public void AddLink(T1 entityA, T2 entityB);
+    public void DeleteLink(T1 entityA, T2 entityB);
+    public IEnumerable<T1> GetLinkedEntities(T2 entity);
+    public IEnumerable<T2> GetLinkedEntities(T1 entity);
 }
--- GraphManipulation/Managers/VacuumingPurposeLinkManager.cs
+++ GraphManipulation/Managers/VacuumingPurposeLinkManager.cs
@@ -5,22 +5,22 @@ namespace GraphManipulation.Managers;
 
 public class VacuumingPurposeLinkManager<T1, T2> : ILinkManager<VacuumingRule<T1>, Purpose<T2>>
 {
-    public void AddLink(VacuumingRule<T1> key1, Purpose<T2> key2)
+    public void AddLink(VacuumingRule<T1> entityA, Purpose<T2> entityB)
     {
         throw new NotImplementedException();
     }
 
-    public void DeleteLink(VacuumingRule<T1> key1, Purpose<T2> key2)
+    public void DeleteLink(VacuumingRule<T1> entityA, Purpose<T2> entityB)
     {
         throw new NotImplementedException();
     }
 
-    public IEnumerable<VacuumingRule<T1>> GetLinkedEntities(Purpose<T2> key)
+    public IEnumerable<VacuumingRule<T1>> GetLinkedEntities(Purpose<T2> entity)
     {
         throw new NotImplementedException();
     }
 
-    public IEnumerable<Purpose<T2>> GetLinkedEntities(VacuumingRule<T1> key)
+    public IEnumerable<Purpose<T2>> GetLinkedEntities(VacuumingRule<T1> entity)
     {
         throw new NotImplementedException();
     }
