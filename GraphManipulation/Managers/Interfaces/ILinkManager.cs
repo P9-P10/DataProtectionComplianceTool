57d65a02d2e0bf985215904140d7fb4258dc7f4c
Ane <anesoejoe@gmail.com>
2023-04-14 16:11:08 +0200

    Renamed parameters and methods to no longer be dependent on order

--- GraphManipulation/Managers/Interfaces/ILinkManager.cs
+++ GraphManipulation/Managers/Interfaces/ILinkManager.cs
@@ -2,8 +2,8 @@ namespace GraphManipulation.Managers.Interfaces;
 
 public interface ILinkManager<T1, T2>
 {
-    public void AddLink(T1 from, T2 to);
-    public void DeleteLink(T1 from, T2 to);
-    public IEnumerable<T1> GetLinkedFromEntities(T2 to);
-    public IEnumerable<T2> GetLinkedToEntities(T1 from);
+    public void AddLink(T1 key1, T2 key2);
+    public void DeleteLink(T1 key1, T2 key2);
+    public IEnumerable<T1> GetLinkedEntities(T2 key);
+    public IEnumerable<T2> GetLinkedEntities(T1 key);
 }
--- GraphManipulation/Managers/VacuumingPurposeLinkManager.cs
+++ GraphManipulation/Managers/VacuumingPurposeLinkManager.cs
@@ -5,22 +5,22 @@ namespace GraphManipulation.Managers;
 
 public class VacuumingPurposeLinkManager<T1, T2> : ILinkManager<VacuumingRule<T1>, Purpose<T2>>
 {
-    public void AddLink(VacuumingRule<T1> from, Purpose<T2> to)
+    public void AddLink(VacuumingRule<T1> key1, Purpose<T2> key2)
     {
         throw new NotImplementedException();
     }
 
-    public void DeleteLink(VacuumingRule<T1> from, Purpose<T2> to)
+    public void DeleteLink(VacuumingRule<T1> key1, Purpose<T2> key2)
     {
         throw new NotImplementedException();
     }
 
-    public IEnumerable<VacuumingRule<T1>> GetLinkedFromEntities(Purpose<T2> to)
+    public IEnumerable<VacuumingRule<T1>> GetLinkedEntities(Purpose<T2> key)
     {
         throw new NotImplementedException();
     }
 
-    public IEnumerable<Purpose<T2>> GetLinkedToEntities(VacuumingRule<T1> from)
+    public IEnumerable<Purpose<T2>> GetLinkedEntities(VacuumingRule<T1> key)
     {
         throw new NotImplementedException();
     }
