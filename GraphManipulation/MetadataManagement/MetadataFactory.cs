36cffde97e8018844e12367858962c17f7eb0cac
Jakob Sønderby Kristensen <33911621+soenderby@users.noreply.github.com>
2023-04-14 09:50:18 +0200

    Create MetadataFactory
    Add factory method 'CreateMetadata' that creates default instance of
    GDPRMetadata.
    Add method 'For' that sets properties TargetTable and TargetColumn
    based on a given instance of ColumnMetadata.

---
+++ GraphManipulation/MetadataManagement/MetadataFactory.cs
@@ -0,0 +1,18 @@
+﻿using GraphManipulation.DataAccess.Entities;
+
+namespace GraphManipulation.MetadataManagement;
+
+public static class MetadataFactory
+{
+    public static GDPRMetadata CreateMetadata()
+    {
+        return new GDPRMetadata();
+    }
+
+    public static GDPRMetadata For(this GDPRMetadata metadata, ColumnMetadata column)
+    {
+        metadata.TargetTable = column.TargetTable;
+        metadata.TargetColumn = column.TargetColumn;
+        return metadata;
+    }
+}
