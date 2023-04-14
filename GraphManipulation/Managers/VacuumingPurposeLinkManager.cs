64c6811f04c972b04a348d34819b19abb23d65f1
Ane <anesoejoe@gmail.com>
2023-04-14 16:26:04 +0200

    Jeg er muligvis gået en smule over gevind. Måske ideen er god?

--- GraphManipulation/Managers/VacuumingPurposeLinkManager.cs
+++ GraphManipulation/Managers/VacuumingPurposeLinkManager.cs
@@ -3,19 +3,59 @@ using GraphManipulation.Models;
 
 namespace GraphManipulation.Managers;
 
-public class VacuumingPurposeLinkManager<T1, T2> : ILinkManager<VacuumingRule<T1>, Purpose<T2>>
+public class VacuumingPurposeLinkManager<T1, T2> : ILinkManager<VacuumingRule<T1>, Purpose<T2>, T1, T2>
 {
-    public void AddLink(VacuumingRule<T1> entityA, Purpose<T2> entityB)
+    public void AddLink(VacuumingRule<T1> entity1, Purpose<T2> entity2)
     {
         throw new NotImplementedException();
     }
 
-    public void DeleteLink(VacuumingRule<T1> entityA, Purpose<T2> entityB)
+    public void AddLink(T1 entity1, Purpose<T2> entity2)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddLink(VacuumingRule<T1> entity1, T2 entity2)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddLink(T1 entity1, T2 entity2)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteLink(VacuumingRule<T1> entity1, Purpose<T2> entity2)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteLink(T1 entity1, Purpose<T2> entity2)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteLink(VacuumingRule<T1> entity1, T2 entity2)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteLink(T1 entity1, T2 entity2)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<VacuumingRule<T1>> GetLinkedEntities(T2 identifier)
     {
         throw new NotImplementedException();
     }
 
     public IEnumerable<VacuumingRule<T1>> GetLinkedEntities(Purpose<T2> entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Purpose<T2>> GetLinkedEntities(T1 identifier)
     {
         throw new NotImplementedException();
     }
