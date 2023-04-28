4af005072c099fbc4d07a4eed6967eb1733da4a8
Jakob Sønderby Kristensen <33911621+soenderby@users.noreply.github.com>
2023-04-28 13:33:53 +0200

    Refactor system test for readability
    Create class that encapsulates the process running the program.
    Add methods that facilitates easy interaction with the program.

--- Test/SystemTest/CommandlineTest.cs
+++ Test/SystemTest/CommandlineTest.cs
@@ -12,8 +12,70 @@ namespace Test.SystemTest;
 
 public class CommandlineTest
 {
-    [Fact]
-    public void TestCommandLine()
+
+    private class TestProcess : IDisposable
+    {
+        private readonly string _executablePath;
+        public Process Process { get; private set; }
+        public List<string> Output { get; private set; }
+
+        public TestProcess(string executablePath)
+        {
+            _executablePath = executablePath;
+            Process = CreateProcess(executablePath);
+            Output = new List<string>();
+        }
+        
+        private Process CreateProcess(string executablePath)
+        {
+            ProcessStartInfo startInfo = CreateProcessStartInfo(executablePath);
+            Process process = new Process();
+            process.StartInfo = startInfo;
+
+            return process;
+        }
+        
+        private ProcessStartInfo CreateProcessStartInfo(string executablePath)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = true;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardInput = true;
+
+            startInfo.UseShellExecute = false;
+            startInfo.Arguments = "";
+            startInfo.FileName = executablePath;
+
+            return startInfo;
+        }
+
+        public void Start()
+        {
+            Process.OutputDataReceived += (sender, args) => Output.Add(args.Data);
+            Process.Start();
+            Process.BeginOutputReadLine();
+        }
+
+        public void GiveInput(string input)
+        {
+            Process.StandardInput.WriteLine(input);
+            Process.WaitForExit(1000);
+        }
+
+        public string GetOutput()
+        {
+            return string.Join("", Output);
+        }
+
+        public void Dispose()
+        {
+            Process.Kill();
+            Process.Dispose();
+        }
+    }
+
+
+    private string CreateConfigFile()
     {
         var configFilePath = Path.Combine(Directory.GetCurrentDirectory(), "config.json");
         var configValues = new Dictionary<string, string>
@@ -27,36 +89,27 @@ public class CommandlineTest
         };
         File.WriteAllText(configFilePath, JsonConvert.SerializeObject( configValues ));
 
+        return configFilePath;
+    }
+    
+    [Fact]
+    public void TestCommandLine()
+    {
+        string configFilePath = CreateConfigFile();
         string executablePath = Path.Combine(Directory.GetCurrentDirectory(), "GraphManipulation.exe");
-        
-        ProcessStartInfo startInfo = new ProcessStartInfo();
-        Process p = new Process();
 
-        startInfo.CreateNoWindow = true;
-        startInfo.RedirectStandardOutput = true;
-        startInfo.RedirectStandardInput = true;
-
-        startInfo.UseShellExecute = false;
-        startInfo.Arguments = "";
-        startInfo.FileName = executablePath;
-
+        TestProcess process = new TestProcess(executablePath);
         try
         {
-            p.StartInfo = startInfo;
-            var output = new List<string>();
-            p.OutputDataReceived += (sender, args) => output.Add(args.Data);
-            p.Start();
-            p.BeginOutputReadLine();
-            p.StandardInput.WriteLine("--help");
-            p.WaitForExit(1000);
+            process.Start();
+            process.GiveInput("--help");
 
-            var result = string.Join("", output);
-
+            string result = process.GetOutput();
             result.Should().Be(@$"Using config found at {configFilePath}Description:  This is a description of the root commandUsage:  ! [command] [options]Options:  --version       Show version information  -?, -h, --help  Show help and usage informationCommands:  ids, individuals  pd, personal-data  ps, purposes  origins, os  vacuuming-rules, vrs  dcs, delete-conditions  processings, prs  lgs, logs  cfg, configuration");
         }
         finally
         {
-            p.Kill();
+            process.Dispose();
         }
     }
 
