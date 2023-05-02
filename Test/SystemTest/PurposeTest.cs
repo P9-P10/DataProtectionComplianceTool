a720deec25228e8a90651bf54d90447b74a16ae2
Ane <anesoejoe@gmail.com>
2023-05-02 10:44:45 +0200

    Auto stash before merge of "dev" and "origin/dev"

---
+++ Test/SystemTest/PurposeTest.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Linq;
+using FluentAssertions;
+using GraphManipulation.Commands.Helpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Test.SystemTest;
+
+public class PurposeTest
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public PurposeTest(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    private const string PurposeName1 = "purposeName1";
+    private const string DeleteConditionName = "deleteConditionName";
+    private const string DeleteCondition = "This is a delete condition";
+
+    private static void AddDeleteCondition(TestProcess testProcess)
+    {
+        const string addDeleteConditionCommand = $"{CommandNamer.DeleteConditionName} {CommandNamer.Add} " +
+                                                 $"{OptionNamer.Name} {DeleteConditionName} " +
+                                                 $"{OptionNamer.Condition} \"{DeleteCondition}\" ";
+        
+        testProcess.GiveInput(addDeleteConditionCommand);
+    }
+
+    private static void AddPurpose(TestProcess testProcess)
+    {
+        const string addPurposeCommand = $"{CommandNamer.PurposesName} {CommandNamer.Add} " +
+                                         $"{OptionNamer.Name} {PurposeName1} " +
+                                         $"{OptionNamer.DeleteConditionName} {DeleteConditionName}";
+        
+        testProcess.GiveInput(addPurposeCommand);
+    }
+
+    private static void ShowPurpose(TestProcess testProcess)
+    {
+        const string showPurposeCommand = $"{CommandNamer.PurposesName} {CommandNamer.Show} " +
+                                          $"{OptionNamer.Name} {PurposeName1}";
+        
+        testProcess.GiveInput(showPurposeCommand);
+    }
+    
+    [Fact]
+    public void AddIsSuccessful()
+    {
+        using var process = SystemTest.CreateTestProcess();
+        process.Start();
+
+        AddDeleteCondition(process);
+        AddPurpose(process);
+
+        process.GetError().Should().BeEmpty();
+        process.GetOutput().Should().Contain($"Successfully added {PurposeName1} purpose");
+    }
+
+    [Fact]
+    public void PurposeCanBeRetrievedAfterAdd()
+    {
+        using var process = SystemTest.CreateTestProcess();
+        process.Start();
+
+        AddDeleteCondition(process);
+        AddPurpose(process);
+        ShowPurpose(process);
+
+        process.GetError().Should().BeEmpty();
+    }
+}
