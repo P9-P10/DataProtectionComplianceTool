232e8987632edb21098db89715bcdbc131820398
Ane <anesoejoe@gmail.com>
2023-05-02 11:39:09 +0200

    Added GetAll output/errors with no whitespace

--- Test/SystemTest/TestProcess.cs
+++ Test/SystemTest/TestProcess.cs
@@ -72,6 +72,11 @@ public class TestProcess : IDisposable
     {
         return FlattenList(AllOutputs);
     }
+    
+    public IEnumerable<string> GetAllOutputNoWhitespace()
+    {
+        return GetAllOutput().Where(s => !string.IsNullOrWhiteSpace(s));
+    }
 
     public List<string> GetLastOutput()
     {
@@ -86,6 +91,11 @@ public class TestProcess : IDisposable
     public List<string> GetAllErrors()
     {
         return FlattenList(AllErrors);
+    }
+
+    public IEnumerable<string> GetAllErrorsNoWhitespace()
+    {
+        return GetAllErrors().Where(s => !string.IsNullOrWhiteSpace(s));
     }
 
     public List<string> GetLastError()
