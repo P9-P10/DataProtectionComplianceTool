d209289abd763419e4dd54b681b1af7ed50940b5
Alexander Nykjær <alax0013@gmail.com>
2023-05-02 11:27:27 +0200

    Added more tests.

--- Test/SystemTest/TestOrigins.cs
+++ Test/SystemTest/TestOrigins.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Collections.Generic;
+using FluentAssertions;
 using GraphManipulation.Commands.Helpers;
 using Xunit;
 
@@ -31,9 +32,28 @@ public class TestOrigins
         const string description = "This is the description";
         const string name = "OriginName";
         process.GiveInput($"{CommandNamer.OriginsAlias} {CommandNamer.AddAlias} {OptionNamer.Name} {name} {OptionNamer.Description} \"{description}\"");
+        process.GiveInput($"{CommandNamer.OriginsAlias} {CommandNamer.List}");
+        List<string> result = process.GetLastOutput();
+
+        result.Should().Contain($"{name}, {description}, [   ]");
+    }
+    // TODO Opdater tests til ikke at bruge .getoutput men getLastoutput i stedet.
+    
+    [Fact]
+    public void TestUpdateOrigin_Returns_Correct_Message()
+    {
+        using TestProcess process = SystemTest.CreateTestProcess();
+        process.Start();
+
+        const string description = "This is the description";
+        const string name = "OriginName";
+        process.GiveInput($"{CommandNamer.OriginsAlias} {CommandNamer.AddAlias} {OptionNamer.Name} {name} {OptionNamer.Description} \"{description}\"");
+        
+        
+        process.GiveInput($"{CommandNamer.OriginsAlias} {CommandNamer.UpdateAlias} {OptionNamer.Name} {name} {OptionNamer.NewName} NewName");
         string result = process.GetOutput();
 
-        result.Should().Contain(description);
+        result.Should().Contain("NewName");
         result.Should().Contain(name);
         result.Should().Contain("Successfully");
     }
