a81a88b188c563896f63452662fe1a953ada63fb
Ane <anesoejoe@gmail.com>
2023-05-02 13:25:54 +0200

    Started personal data system test

---
+++ Test/SystemTest/PersonalDataTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Test.SystemTest;
+
+[Collection("SystemTestSequential")]
+public class PersonalDataTest
+{
+    [Fact]
+    public void AddIsSuccessful()
+    {
+        
+    }
+}
