44e28734b74a12c4d771ec1a0fc507f9e5235ea6
Ane <anesoejoe@gmail.com>
2023-05-02 13:46:15 +0200

    More personal data system tests

--- Test/SystemTest/PersonalDataTest.cs
+++ Test/SystemTest/PersonalDataTest.cs
@@ -3,11 +3,15 @@ using Xunit;
 namespace Test.SystemTest;
 
 [Collection("SystemTestSequential")]
-public class PersonalDataTest
+public class PersonalDataTest : TestResources
 {
     [Fact]
     public void AddIsSuccessful()
     {
-        
+        using var process = SystemTest.CreateTestProcess();
+        process.Start();
+
+        AddDeleteCondition(process, TestDeleteCondition);
+        AddPurpose(process, TestPurpose);
     }
 }
