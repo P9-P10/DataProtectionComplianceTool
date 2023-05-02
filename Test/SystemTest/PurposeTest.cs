59abf5c18bdf7b40ee102c2d0ca6635d737eb4a7
Ane <anesoejoe@gmail.com>
2023-05-02 13:40:36 +0200

    Refactor to allow code reuse

--- Test/SystemTest/PurposeTest.cs
+++ Test/SystemTest/PurposeTest.cs
@@ -1,107 +1,15 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using FluentAssertions;
 using GraphManipulation.Commands.Helpers;
-using GraphManipulation.Managers;
-using GraphManipulation.Models;
 using GraphManipulation.Models.Interfaces;
 using Xunit;
-using Xunit.Abstractions;
 
 namespace Test.SystemTest;
 
 [Collection("SystemTestSequential")]
-public class PurposeTest
+public class PurposeTest : TestResources
 {
-    private const string Description = "This is a description";
-
-    private static readonly DeleteCondition TestDeleteCondition = new()
-    {
-        Name = "deleteConditionName",
-        Description = Description,
-        Condition = "This is a condition"
-    };
-
-    private static readonly DeleteCondition NewTestDeleteCondition = new()
-    {
-        Name = TestDeleteCondition.GetName() + "NEW",
-        Description = TestDeleteCondition.GetDescription() + "NEW",
-        Condition = TestDeleteCondition.GetCondition() + "NEW"
-    };
-
-    private static readonly Purpose TestPurpose = new()
-    {
-        Name = "purposeName",
-        Description = Description,
-        DeleteCondition = TestDeleteCondition,
-        LegallyRequired = true,
-        Columns = new List<PersonalDataColumn>(),
-        Rules = new List<VacuumingRule>()
-    };
-
-    private static readonly Purpose NewTestPurpose = new()
-    {
-        Name = TestPurpose.GetName() + "NEW",
-        Description = Description + "NEW",
-        DeleteCondition = NewTestDeleteCondition,
-        LegallyRequired = !TestPurpose.GetLegallyRequired(),
-        Columns = new List<PersonalDataColumn>(),
-        Rules = new List<VacuumingRule>()
-    };
-
-    private static void AddDeleteCondition(TestProcess testProcess, IDeleteCondition deleteCondition)
-    {
-        var addDeleteConditionCommand = $"{CommandNamer.DeleteConditionName} {CommandNamer.Add} " +
-                                        $"{OptionNamer.Name} {deleteCondition.GetName()} " +
-                                        $"{OptionNamer.Condition} \"{deleteCondition.GetCondition()}\" " +
-                                        $"{OptionNamer.Description} \"{deleteCondition.GetDescription()}\"";
-        
-        testProcess.GiveInput(addDeleteConditionCommand);
-    }
-
-    private static void AddPurpose(TestProcess testProcess, IPurpose purpose)
-    {
-        var addPurposeCommand = $"{CommandNamer.PurposesName} {CommandNamer.Add} " +
-                                         $"{OptionNamer.Name} {purpose.GetName()} " +
-                                         $"{OptionNamer.Description} \"{purpose.GetDescription()}\" " +
-                                         $"{OptionNamer.DeleteConditionName} {purpose.GetDeleteCondition()} " +
-                                         $"{OptionNamer.LegallyRequired} {purpose.GetLegallyRequired()} ";
-
-        testProcess.GiveInput(addPurposeCommand);
-    }
-
-    private static void ShowPurpose(TestProcess testProcess, IPurpose purpose)
-    {
-        var showPurposeCommand = $"{CommandNamer.PurposesName} {CommandNamer.Show} " + 
-                                 $"{OptionNamer.Name} {purpose.GetName()}";
-        
-        testProcess.GiveInput(showPurposeCommand);
-    }
-
-    private static void UpdatePurpose(TestProcess testProcess)
-    {
-        var updatePurposeCommand = $"{CommandNamer.PurposesName} {CommandNamer.Update} " +
-                                   $"{OptionNamer.Name} {TestPurpose.GetName()} " +
-                                   $"{OptionNamer.Description} \"{NewTestPurpose.GetDescription()}\" " +
-                                   $"{OptionNamer.LegallyRequired} {NewTestPurpose.GetLegallyRequired()} " +
-                                   $"{OptionNamer.DeleteConditionName} {NewTestPurpose.GetDeleteCondition()} " +
-                                   $"{OptionNamer.NewName} {NewTestPurpose.GetName()}";
-        
-        testProcess.GiveInput(updatePurposeCommand);
-    }
-
-    private static void ListPurpose(TestProcess testProcess)
-    {
-        testProcess.GiveInput($"{CommandNamer.PurposesName} {CommandNamer.List}");
-    }
-
-    private static void DeletePurpose(TestProcess testProcess, IPurpose purpose)
-    {
-        var deleteCommand = $"{CommandNamer.PurposesName} {CommandNamer.Delete} " +
-                            $"{OptionNamer.Name} {purpose.GetName()}";
-        testProcess.GiveInput(deleteCommand);
-    }
+    
     
     [Fact]
     public void AddIsSuccessful()
@@ -119,7 +27,7 @@ public class PurposeTest
         output.Should().ContainSingle(s => 
             s.Contains($"Successfully added {TestPurpose.GetName()} purpose") &&
             s.Contains($"{TestPurpose.GetLegallyRequired()}") && 
-            s.Contains(Description)
+            s.Contains(TestPurpose.GetDescription())
             );
         output.Should().ContainSingle(s =>
             s.Contains($"Successfully updated {TestPurpose.GetName()} purpose with {TestDeleteCondition.GetName()}"));
@@ -151,7 +59,7 @@ public class PurposeTest
         AddDeleteCondition(process, TestDeleteCondition);
         AddDeleteCondition(process, NewTestDeleteCondition);
         AddPurpose(process, TestPurpose);
-        UpdatePurpose(process);
+        UpdatePurpose(process, TestPurpose, NewTestPurpose);
         ShowPurpose(process, NewTestPurpose);
         
         var error = process.GetAllErrorsNoWhitespace();
@@ -189,7 +97,6 @@ public class PurposeTest
 
         AddDeleteCondition(process, TestDeleteCondition);
         AddPurpose(process, TestPurpose);
-        ListPurpose(process);
         DeletePurpose(process, TestPurpose);
         
         var error = process.GetAllErrorsNoWhitespace();
