db0b02eda801794c2e28742da490488b4f4f71e5
Alexander Nykjær <alax0013@gmail.com>
2023-05-02 14:02:52 +0200

    We now have a integrationstests project.

--- IntegrationTests/SystemTest/PersonalDataTest.cs
+++ IntegrationTests/SystemTest/PersonalDataTest.cs
@@ -1,5 +1,3 @@
-using Xunit;
-
 namespace Test.SystemTest;
 
 [Collection("SystemTestSequential")]
--- IntegrationTests/SystemTest/TestOrigins.cs
+++ IntegrationTests/SystemTest/TestOrigins.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using FluentAssertions;
+﻿using FluentAssertions;
 using GraphManipulation.Commands.Helpers;
-using Xunit;
 
 namespace Test.SystemTest;
 
