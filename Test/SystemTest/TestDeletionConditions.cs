a674e162c8efaed45345f56693ff8e5040dc1ad2
Alexander Nykjær <alax0013@gmail.com>
2023-05-02 09:42:24 +0200

    Making TestDeletionCondition

---
+++ Test/SystemTest/TestDeletionConditions.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Test.SystemTest;
+
+public class TestDeletionConditions
+{
+    [Fact]
+    public void TestAddCommand()
+    {
+        using TestProcess process = SystemTest.CreateTestProcess();
+        process.Start();
+
+        process.GiveInput("dcs a -n DeletionCondition -c \"Condition\"");
+        string result = process.GetOutput();
+        string error = process.GetError();
+        result.Should()
+            .Be(
+                $@"Using config found at {SystemTest.DefaultConfigPath}" +
+                "$: Successfully added DeletionCondition entity with , Condition");
+    }
+}
