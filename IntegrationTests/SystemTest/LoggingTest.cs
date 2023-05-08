baa230037cd836442584b8a73b1e18c49ef5a213
Ane <anesoejoe@gmail.com>
2023-05-08 13:32:34 +0200

    Refactor logging system test to prepare for more tests

--- IntegrationTests/SystemTest/LoggingTest.cs
+++ IntegrationTests/SystemTest/LoggingTest.cs
@@ -4,7 +4,6 @@ using IntegrationTests.SystemTest.Tools;
 
 namespace IntegrationTests.SystemTest;
 
-
 // TODO: Test that vacuuming executions gets logged correctly
 
 public class LoggingTest : TestResources
@@ -16,132 +15,161 @@ public class LoggingTest : TestResources
             AddLogEntryOrigin(testProcess, i.ToString());
         }
     }
-    
-    [Fact]
-    public void OneLogEntryOneOutput()
+
+    private static IEnumerable<string> FormatOutputForLogTest(IEnumerable<string> list)
     {
-        using var process = Tools.SystemTest.CreateTestProcess();
-        process.Start();
-
-        const int numberOfEntries = 1;
-        CreateLogEntries(process, numberOfEntries);
-        
-        var constraints = new LogConstraints(limit: numberOfEntries + 1);
-        
-        ListLogs(process, constraints);
-        
-        var error = process.GetAllErrorsNoWhitespace();
-        var output = process
-            .GetAllOutputNoWhitespace()
-            .Select(s => s.Replace("$:", "").Trim())
-            .ToList();
-
-        error.Should().BeEmpty();
-        output.Last().Should().Be($"Showing all {numberOfEntries} log entries");
-
-        var logEntries = output
-            .TakeLast(numberOfEntries + 1)
-            .Take(numberOfEntries)
-            .Select(s => new Log(s))
-            .ToList();
-        
-        logEntries.First().LogNumber.Should().Be(1);
-        logEntries.Last().LogNumber.Should().Be(numberOfEntries);
+        return list.Select(s => s.Replace("$:", "").Trim());
     }
 
-    [Fact]
-    public void ManyEntriesManyOutputs()
+    public class Basic
     {
-        using var process = Tools.SystemTest.CreateTestProcess();
-        process.Start();
+        [Fact]
+        public void OneLogEntryOneOutput()
+        {
+            using var process = Tools.SystemTest.CreateTestProcess();
+            process.Start();
 
-        const int numberOfEntries = 10;
-        CreateLogEntries(process, numberOfEntries);
+            const int numberOfEntries = 1;
+            CreateLogEntries(process, numberOfEntries);
+
+            var constraints = new LogConstraints(limit: numberOfEntries + 1);
+
+            ListLogs(process, constraints);
+
+            var error = process.GetAllErrorsNoWhitespace();
+            var output = FormatOutputForLogTest(process.GetAllOutputNoWhitespace()).ToList();
+
+            error.Should().BeEmpty();
+            output.Last().Should().Be($"Showing all {numberOfEntries} log entries");
+
+            var logEntries = output
+                .TakeLast(numberOfEntries + 1)
+                .Take(numberOfEntries)
+                .Select(s => new Log(s))
+                .ToList();
+
+            logEntries.First().LogNumber.Should().Be(1);
+            logEntries.Last().LogNumber.Should().Be(numberOfEntries);
+        }
+
+        [Fact]
+        public void ManyEntriesManyOutputs()
+        {
+            using var process = Tools.SystemTest.CreateTestProcess();
+            process.Start();
+
+            const int numberOfEntries = 10;
+            CreateLogEntries(process, numberOfEntries);
+
+            var constraints = new LogConstraints(limit: numberOfEntries + 1);
+
+            ListLogs(process, constraints);
+
+            var error = process.GetAllErrorsNoWhitespace();
+            var output = FormatOutputForLogTest(process.GetAllOutputNoWhitespace()).ToList();
+
+            error.Should().BeEmpty();
+            output.Last().Should().Be($"Showing all {numberOfEntries} log entries");
+
+            var logEntries = output
+                .TakeLast(numberOfEntries + 1)
+                .Take(numberOfEntries)
+                .Select(s => new Log(s))
+                .ToList();
+
+            logEntries.First().LogNumber.Should().Be(1);
+            logEntries.Last().LogNumber.Should().Be(numberOfEntries);
+        }
+
+        [Fact]
+        public void ManyEntriesLimitedOutputChangesMessage()
+        {
+            using var process = Tools.SystemTest.CreateTestProcess();
+            process.Start();
+
+            const int numberOfEntries = 10;
+            CreateLogEntries(process, numberOfEntries);
+
+            var constraints = new LogConstraints(limit: numberOfEntries);
+
+            ListLogs(process, constraints);
+
+            var error = process.GetAllErrorsNoWhitespace();
+            var output = FormatOutputForLogTest(process.GetAllOutputNoWhitespace()).ToList();
+
+            error.Should().BeEmpty();
+            output.Last().Should().Be($"Showing newest {constraints.Limit} log entries");
+
+            var logEntries = output
+                .TakeLast(numberOfEntries + 1)
+                .Take(numberOfEntries)
+                .Select(s => new Log(s))
+                .ToList();
+
+            logEntries.First().LogNumber.Should().Be(1);
+            logEntries.Last().LogNumber.Should().Be(numberOfEntries);
+        }
+
+        [Fact]
+        public void ManyEntriesLimitedOutputChangesLogsShown()
+        {
+            using var process = Tools.SystemTest.CreateTestProcess();
+            process.Start();
+
+            const int numberOfEntries = 10;
+            CreateLogEntries(process, numberOfEntries);
+
+            var constraints = new LogConstraints(limit: numberOfEntries - 3);
+
+            ListLogs(process, constraints);
+
+            var error = process.GetAllErrorsNoWhitespace();
+            var output = FormatOutputForLogTest(process.GetAllOutputNoWhitespace()).ToList();
+
+            error.Should().BeEmpty();
+            output.Last().Should().Be($"Showing newest {constraints.Limit} log entries");
+
+            var logEntries = output
+                .TakeLast(constraints.Limit + 1)
+                .Take(constraints.Limit)
+                .Select(s => new Log(s))
+                .ToList();
+
+            logEntries.First().LogNumber.Should().Be(numberOfEntries - constraints.Limit + 1);
+            logEntries.Last().LogNumber.Should().Be(numberOfEntries);
+        }
+    }
+
+    public class Metadata
+    {
         
-        var constraints = new LogConstraints(limit: numberOfEntries + 1);
-        
-        ListLogs(process, constraints);
-        
-        var error = process.GetAllErrorsNoWhitespace();
-        var output = process
-            .GetAllOutputNoWhitespace()
-            .Select(s => s.Replace("$:", "").Trim())
-            .ToList();
-        
-        error.Should().BeEmpty();
-        output.Last().Should().Be($"Showing all {numberOfEntries} log entries");
-        
-        var logEntries = output
-            .TakeLast(numberOfEntries + 1)
-            .Take(numberOfEntries)
-            .Select(s => new Log(s))
-            .ToList();
-        
-        logEntries.First().LogNumber.Should().Be(1);
-        logEntries.Last().LogNumber.Should().Be(numberOfEntries);
     }
     
-    [Fact]
-    public void ManyEntriesLimitedOutputChangesMessage()
+    public class System
     {
-        using var process = Tools.SystemTest.CreateTestProcess();
-        process.Start();
-
-        const int numberOfEntries = 10;
-        CreateLogEntries(process, numberOfEntries);
         
-        var constraints = new LogConstraints(limit: numberOfEntries);
-        
-        ListLogs(process, constraints);
-        
-        var error = process.GetAllErrorsNoWhitespace();
-        var output = process
-            .GetAllOutputNoWhitespace()
-            .Select(s => s.Replace("$:", "").Trim())
-            .ToList();
-        
-        error.Should().BeEmpty();
-        output.Last().Should().Be($"Showing newest {constraints.Limit} log entries");
-        
-        var logEntries = output
-            .TakeLast(numberOfEntries + 1)
-            .Take(numberOfEntries)
-            .Select(s => new Log(s))
-            .ToList();
-        
-        logEntries.First().LogNumber.Should().Be(1);
-        logEntries.Last().LogNumber.Should().Be(numberOfEntries);
     }
-    
-    [Fact]
-    public void ManyEntriesLimitedOutputChangesLogsShown()
-    {
-        using var process = Tools.SystemTest.CreateTestProcess();
-        process.Start();
 
-        const int numberOfEntries = 10;
-        CreateLogEntries(process, numberOfEntries);
-        
-        var constraints = new LogConstraints(limit: numberOfEntries - 3);
-        
-        ListLogs(process, constraints);
-        
-        var error = process.GetAllErrorsNoWhitespace();
-        var output = process
-            .GetAllOutputNoWhitespace()
-            .Select(s => s.Replace("$:", "").Trim())
-            .ToList();
-        
-        error.Should().BeEmpty();
-        output.Last().Should().Be($"Showing newest {constraints.Limit} log entries");
-        
-        var logEntries = output
-            .TakeLast(constraints.Limit + 1)
-            .Take(constraints.Limit)
-            .Select(s => new Log(s))
-            .ToList();
-        
-        logEntries.First().LogNumber.Should().Be(numberOfEntries - constraints.Limit + 1);
-        logEntries.Last().LogNumber.Should().Be(numberOfEntries);
+    public class Vacuuming
+    {
+        [Fact]
+        public void ExecutingVacuumingRuleResultsInLogEntries()
+        {
+            using var process = Tools.SystemTest.CreateTestProcess();
+            process.Start();
+            
+            AddDeleteCondition(process, TestDeleteCondition);
+            AddPurpose(process, TestPurpose);
+            AddPersonalData(process, TestPersonalDataColumn);
+            AddVacuumingRule(process, TestVacuumingRule);
+            ExecuteVacuumingRule(process, new[] { TestVacuumingRule });
+            
+            var constraints = new LogConstraints(logTypes: new [] { LogType.Vacuuming });
+            
+            ListLogs(process, constraints);
+
+            var error = process.GetAllErrorsNoWhitespace();
+            var output = FormatOutputForLogTest(process.GetAllOutputNoWhitespace()).ToList();
+        }
     }
 }
