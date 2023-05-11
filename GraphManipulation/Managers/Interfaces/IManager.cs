32d3c781aba4a441453bb029cd9e63364ea102e4
Ane Jørgensen <anesoejoe@gmail.com>
2023-05-11 13:48:50 +0200

    Removed unnecessary type constraint

--- GraphManipulation/Managers/Interfaces/IManager.cs
+++ GraphManipulation/Managers/Interfaces/IManager.cs
@@ -5,8 +5,6 @@ using GraphManipulation.Models.Base;
 namespace GraphManipulation.Managers.Interfaces;
 
 public interface IManager<TKey, TValue> : IGetter<TValue, TKey> 
-    where TKey : notnull
-    where TValue : Entity<TKey>, new()
 {
     public bool Create(TKey key);
     public bool Update(TKey key, TValue value);
